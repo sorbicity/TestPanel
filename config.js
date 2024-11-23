275d6e7d851707f177f306c0ea275e273f9b06dd
sorbicity <sorbicity@post.com>
2024-11-23 01:45:27 -0700

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const config = {
     GIST_ID: '9bcbcec8844698d54632f7772f3eb081',
-    GITHUB_TOKEN: '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'
+    TOKEN_GITHUB: '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'
 };
